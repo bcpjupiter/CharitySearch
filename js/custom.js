d437458686281284a1e7e8cf4cb14a52c9f44c0b
bcpjupiter <bcpjupiter@gmail.com>
2021-01-31 14:34:09 -0600

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(100); // set duration in brackets
+    });
+
+
+    // MENU
+    $('.menu-burger').on('click', function() {
+      $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+      $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+    });
+
+
+    // ABOUT SLIDER
+    $('body').vegas({
+        slides: [
+            { src: 'images/ocean.png' },
+        ],
+        timer: false,
+        transition: [ 'zoomOut', ]
+    });
+
+})(jQuery);
